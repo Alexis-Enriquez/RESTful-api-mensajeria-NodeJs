4ef21b474c935af3d72ad162bef04c596dfc1125
Alexis Enriquez <alxs.enriquez@gmail.com>
2020-11-01 00:14:25 -0300

    second

--- components/user/controller.js
+++ components/user/controller.js
@@ -29,39 +29,9 @@ function addUser(name){
         resolve(store.list(filterUser))
     })
 }
-/*
-function updateMessage(id,message){
-    return new Promise(async (resolve,reject)=>{
-    if(!id || !message){
-        reject('invalid data')
-        return false
-    }
-
-    const result = await store.update(id, message)
-    resolve(result)
-    })
-}
-
-function deleteMessage(id){
-    return new Promise((resolve,reject)=>{
-        
-        if(!id){
-            reject('no se ha enviado un id')
-        }
-        store.remove(id)
-        .then(()=>{
-            resolve()
-        })
-        .catch(e =>{
-            reject(e)
-        })
-        
-    })
-} */
 
 module.exports = {
     addUser,
      getUsers,
-    /* updateMessage,
-    deleteMessage */
+
 }
